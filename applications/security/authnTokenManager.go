aa86c7d35978dcb10bf2589c74fdcc6caa521770
faishalshidqi <rshidqi6@gmail.com>
2025-02-09 19:09:43 +0700

    updated authnTokenManager.go with GetBearerToken function

--- applications/security/authnTokenManager.go
+++ applications/security/authnTokenManager.go
@@ -2,10 +2,12 @@ package security
 
 import (
 	"forum-api/domains"
+	"net/http"
 	"time"
 )
 
 type AuthnTokenManager interface {
 	CreateToken(user domains.User, secret string, expiresIn time.Duration) (string, error)
-	VerifyToken(token string, secret string) (string, error)
+	VerifyToken(tokenString string, secret string) (string, error)
+	GetBearerToken(header http.Header) (string, error)
 }
