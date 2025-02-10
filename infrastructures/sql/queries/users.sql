2e67f70bd21988c866b84aa708f20e1b0a6b5e62
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 01:11:32 +0700

    added get users, get user by username, and get user by id query

--- infrastructures/sql/queries/users.sql
+++ infrastructures/sql/queries/users.sql
@@ -1,2 +1,11 @@
 -- name: CreateUser :one
 insert into users (id, username, password, name, created_at, updated_at) values(gen_random_uuid(), lower(@username), @password, lower(@name), now(), now()) returning id, username, name;
+
+-- name: GetUsers :many
+select * from users;
+
+-- name: GetByUsername :one
+select * from users where username = @username;
+
+-- name: GetByID :one
+select * from users where id = @id;
