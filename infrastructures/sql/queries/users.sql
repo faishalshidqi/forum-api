3eb1abdbc5e152768e75cde9705b3664d0b14600
faishalshidqi <rshidqi6@gmail.com>
2025-02-10 23:04:48 +0700

    updated Create User query

--- infrastructures/sql/queries/users.sql
+++ infrastructures/sql/queries/users.sql
@@ -1,2 +1,2 @@
 -- name: CreateUser :one
-insert into users (id, username, password, name, created_at, updated_at) values(gen_random_uuid(), $1, $2, $3, now(), now()) returning id, username, name;
+insert into users (id, username, password, name, created_at, updated_at) values(gen_random_uuid(), lower(@username), @password, lower(@name), now(), now()) returning id, username, name;
