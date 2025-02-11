3f75f71acfc92ca537a032931c8d940de9d09f4f
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 14:25:24 +0700

    added docs for signup controller

--- interfaces/http/api/controllers/signupController.go
+++ interfaces/http/api/controllers/signupController.go
@@ -15,6 +15,21 @@ type SignupController struct {
 	Env           *bootstrap.Env
 }
 
+// Signup AddUser godoc
+//
+//	@Summary		Register A User
+//	@Description	New user must have a unique email address
+//	@Tags			users
+//	@Accept			json
+//	@Produce		json
+//	@Param			email		body		string	true	"email address of the new user, must be unique"	Format(email)
+//	@Param			password	body		string	true	"password of the new user"
+//	@Param			name		body		string	true	"name of the new user"
+//	@Success		201			{object}	domains.SignupResponse
+//	@Failure		400			{object}	domains.ErrorResponse
+//	@Failure		409			{object}	domains.ErrorResponse
+//	@Failure		500			{object}	domains.ErrorResponse
+//	@Router			/users [post]
 func (sc *SignupController) Signup(c *gin.Context) {
 	request := &domains.SignupRequest{}
 	if err := c.ShouldBind(request); err != nil {
