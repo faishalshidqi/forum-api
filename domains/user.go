e51e839af59760d0f7dbe5125dc80dfdb676f200
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 13:10:44 +0700

    removed domains.User struct

--- domains/user.go
+++ domains/user.go
@@ -2,21 +2,13 @@ package domains
 
 import (
 	"context"
-	"time"
+	"forum-api/commons/sql/database"
+	"github.com/jackc/pgx/v5/pgtype"
 )
-
-type User struct {
-	ID        string    `json:"id"`
-	Username  string    `json:"username" binding:"required"`
-	Password  string    `json:"password" binding:"required"`
-	FullName  string    `json:"fullname" binding:"required"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
 
 type UserRepository interface {
 	Add(ctx context.Context, user SignupRequest) (SignupResponseData, error)
-	Fetch(ctx context.Context) ([]User, error)
-	GetByUsername(ctx context.Context, username string) (User, error)
-	GetByID(ctx context.Context, id string) (User, error)
+	Fetch(ctx context.Context) ([]database.User, error)
+	GetByUsername(ctx context.Context, username string) (database.User, error)
+	GetByID(ctx context.Context, id pgtype.UUID) (database.User, error)
 }
