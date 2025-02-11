1d916aad7887b81b66685c2b68126f03063fe5bd
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 13:24:50 +0700

    updated name to fullname in users table schema

--- infrastructures/sql/schema/20250210135346_users.sql
+++ infrastructures/sql/schema/20250210135346_users.sql
@@ -4,7 +4,7 @@ create table users (
     id uuid primary key,
     username text not null,
     password text not null,
-    name text not null,
+    fullname text not null,
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp
 );
