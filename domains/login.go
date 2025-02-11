03c1b00d58eefd5ab7530113200fbe05b4a94f13
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 14:35:45 +0700

    updated get user by email to get user by username

--- domains/login.go
+++ domains/login.go
@@ -6,7 +6,7 @@ import (
 )
 
 type LoginUsecase interface {
-	GetUserByEmail(c context.Context, username string) (database.User, error)
+	GetUserByUsername(c context.Context, username string) (database.User, error)
 	CreateAccessToken(user database.User, secret string, expiry int) (accessToken string, err error)
 	CreateRefreshToken(user database.User, secret string, expiry int) (refreshToken string, err error)
 }
