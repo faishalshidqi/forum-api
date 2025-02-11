c4424379bbc90b6b1bbc5280ffbb47ac7684ad9d
faishalshidqi <rshidqi6@gmail.com>
2025-02-11 14:37:23 +0700

    added login request struct

--- domains/login.go
+++ domains/login.go
@@ -5,6 +5,11 @@ import (
 	"forum-api/commons/sql/database"
 )
 
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 type LoginUsecase interface {
 	GetUserByUsername(c context.Context, username string) (database.User, error)
 	CreateAccessToken(user database.User, secret string, expiry int) (accessToken string, err error)
