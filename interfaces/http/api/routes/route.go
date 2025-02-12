5ba8928b7ecf2801e40af7269ef0ec5cd391e879
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 19:08:05 +0700

    added login router at POST /authentications to route config

--- interfaces/http/api/routes/route.go
+++ interfaces/http/api/routes/route.go
@@ -9,4 +9,5 @@ import (
 func Setup(env *bootstrap.Env, timeout time.Duration, db bootstrap.Database, gin *gin.Engine) {
 	router := gin.Group("")
 	newSignupRouter(env, timeout, db, router)
+	newAuthnRouter(env, timeout, db, router)
 }
