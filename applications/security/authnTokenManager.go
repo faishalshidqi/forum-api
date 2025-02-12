42bd24fb58cb699853620ad5b20f5fa8aaad7041
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 14:44:36 +0700

    updated authnTokenManager.go for better compliance to clean architecture

--- applications/security/authnTokenManager.go
+++ applications/security/authnTokenManager.go
@@ -1,13 +1,13 @@
 package security
 
 import (
-	"forum-api/commons/sql/database"
+	"forum-api/domains"
 	"net/http"
 	"time"
 )
 
 type AuthnTokenManager interface {
-	CreateToken(user database.User, secret string, expiresIn time.Duration) (string, error)
+	CreateToken(user domains.User, secret string, expiresIn time.Duration) (string, error)
 	VerifyToken(tokenString string, secret string) (string, error)
 	GetBearerToken(header http.Header) (string, error)
 }
