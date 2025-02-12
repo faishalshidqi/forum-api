bbe616e1aa7049ef0030296120d3e9cac9b59fa6
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 19:12:56 +0700

    updated authentication router

--- interfaces/http/api/routes/authenticationRouter.go
+++ interfaces/http/api/routes/authenticationRouter.go
@@ -13,8 +13,9 @@ import (
 func newAuthnRouter(env *bootstrap.Env, timeout time.Duration, db bootstrap.Database, router *gin.RouterGroup) {
 	userRepository := repository.NewPostgresUserRepository(db)
 	tokenManager := security.NewJwtTokenManager()
+	passwordHash := security.NewBcryptPasswordHash()
 	authenticationController := controllers.AuthenticationController{
-		AuthenticationUsecase: usecase.NewAuthenticationUsecase(userRepository, tokenManager, timeout),
+		AuthenticationUsecase: usecase.NewAuthenticationUsecase(userRepository, tokenManager, passwordHash, timeout),
 		Env:                   env,
 	}
 	router.POST("/authentications", authenticationController.Login)
