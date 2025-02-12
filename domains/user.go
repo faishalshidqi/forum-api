e1b9982c598294659f9d06b39b7ba766238fe112
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 14:38:07 +0700

    rewrote user.go for better compliance to clean architecture

--- domains/user.go
+++ domains/user.go
@@ -2,13 +2,21 @@ package domains
 
 import (
 	"context"
-	"forum-api/commons/sql/database"
-	"github.com/jackc/pgx/v5/pgtype"
+	"time"
 )
+
+type User struct {
+	ID        string    `json:"id"`
+	Username  string    `json:"username" binding:"required"`
+	Password  string    `json:"password" binding:"required"`
+	FullName  string    `json:"fullname" binding:"required"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 type UserRepository interface {
 	Add(ctx context.Context, user SignupRequest) (SignupResponseData, error)
-	Fetch(ctx context.Context) ([]database.User, error)
-	GetByUsername(ctx context.Context, username string) (database.User, error)
-	GetByID(ctx context.Context, id pgtype.UUID) (database.User, error)
+	Fetch(ctx context.Context) ([]User, error)
+	GetByUsername(ctx context.Context, username string) (User, error)
+	GetByID(ctx context.Context, id string) (User, error)
 }
