032732b207b204f617bf67fbc399ffe475da73c9
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 14:49:37 +0700

    implemented get user by id and validate token in authentication use case

--- applications/usecase/authenticationUsecase.go
+++ applications/usecase/authenticationUsecase.go
@@ -13,17 +13,23 @@ type authenticationUsecase struct {
 	contextTimeout time.Duration
 }
 
-func (au authenticationUsecase) GetUserByID(c context.Context, id string) (domains.User, error) {
-	//TODO implement me
-	panic("implement me")
+func (au *authenticationUsecase) GetUserByID(c context.Context, id string) (domains.User, error) {
+	user, err := au.userRepository.GetByID(c, id)
+	if err != nil {
+		return domains.User{}, err
+	}
+	return user, nil
 }
 
-func (au authenticationUsecase) ValidateToken(token, secret string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (au *authenticationUsecase) ValidateToken(token, secret string) (string, error) {
+	id, err := au.tokenManager.VerifyToken(token, secret)
+	if err != nil {
+		return "", err
+	}
+	return id, nil
 }
 
-func (au authenticationUsecase) GetUserByUsername(c context.Context, username string) (domains.User, error) {
+func (au *authenticationUsecase) GetUserByUsername(c context.Context, username string) (domains.User, error) {
 	user, err := au.userRepository.GetByUsername(c, username)
 	if err != nil {
 		return domains.User{}, err
@@ -31,18 +37,18 @@ func (au authenticationUsecase) GetUserByUsername(c context.Context, username st
 	return user, nil
 }
 
-func (au authenticationUsecase) CreateAccessToken(user domains.User, secret string, expiry int) (accessToken string, err error) {
+func (au *authenticationUsecase) CreateAccessToken(user domains.User, secret string, expiry int) (accessToken string, err error) {
 	accessToken, err = au.tokenManager.CreateToken(user, secret, time.Duration(expiry)*time.Hour)
 	return
 }
 
-func (au authenticationUsecase) CreateRefreshToken(user domains.User, secret string, expiry int) (refreshToken string, err error) {
+func (au *authenticationUsecase) CreateRefreshToken(user domains.User, secret string, expiry int) (refreshToken string, err error) {
 	refreshToken, err = au.tokenManager.CreateToken(user, secret, time.Duration(expiry)*time.Hour)
 	return
 }
 
 func NewAuthenticationUsecase(userRepository domains.UserRepository, tokenManager security.AuthnTokenManager, timeout time.Duration) domains.AuthenticationUsecase {
-	return authenticationUsecase{
+	return &authenticationUsecase{
 		userRepository: userRepository,
 		tokenManager:   tokenManager,
 		contextTimeout: timeout,
