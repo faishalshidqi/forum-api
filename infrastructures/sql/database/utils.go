47c71585d106e8104b64757d9fd400f27c5d0f6b
faishalshidqi <rshidqi6@gmail.com>
2025-02-12 14:41:25 +0700

    added utils.go

---
+++ infrastructures/sql/database/utils.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"forum-api/domains"
+)
+
+func (u *User) ToDomainsUser() domains.User {
+	return domains.User{
+		ID:        u.ID.String(),
+		Username:  u.Username,
+		Password:  u.Password,
+		FullName:  u.Fullname,
+		CreatedAt: u.CreatedAt.Time,
+		UpdatedAt: u.UpdatedAt.Time,
+	}
+}
+
+func (cur *CreateUserRow) ToSignupResponseData() domains.SignupResponseData {
+	return domains.SignupResponseData{
+		ID:       cur.ID.String(),
+		Username: cur.Username,
+		FullName: cur.Fullname,
+	}
+}
