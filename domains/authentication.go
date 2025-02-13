afb818e45fcb59c4938e35b7e53b7ff31814db8c
faishalshidqi <rshidqi6@gmail.com>
2025-02-13 13:59:30 +0700

    added refresh token repository interface

--- domains/authentication.go
+++ domains/authentication.go
@@ -40,3 +40,9 @@ type AuthenticationUsecase interface {
 	ValidateToken(token, secret string) (string, error)
 	CheckPasswordHash(password, hash string) error
 }
+
+type RefreshTokenRepository interface {
+	Add(c context.Context, refreshRequest RefreshRequest) error
+	Fetch(c context.Context, refreshToken string) (RefreshRequest, error)
+	Delete(c context.Context, refreshToken string) error
+}
