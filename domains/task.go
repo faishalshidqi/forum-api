cb3b307b43f17b28ccd5c135fc1d0091bf3ace77
faishalshidqi <rshidqi6@gmail.com>
2025-02-14 07:49:26 +0700

    updated task struct

--- domains/task.go
+++ domains/task.go
@@ -1,11 +1,13 @@
 package domains
 
-import "time"
+import (
+	"time"
+)
 
 type Task struct {
-	ID       string    `json:"id"`
-	Title    string    `json:"title" binding:"required"`
-	Body     string    `json:"body" binding:"required"`
-	Date     time.Time `json:"date"`
-	Username string    `json:"Username"`
+	ID    string    `json:"id"`
+	Title string    `json:"title" binding:"required"`
+	Body  string    `json:"body" binding:"required"`
+	Date  time.Time `json:"date"`
+	Owner string    `json:"owner"`
 }
